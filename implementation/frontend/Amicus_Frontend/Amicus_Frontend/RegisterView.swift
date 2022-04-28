e72966e4e70bb6becff8cfb5a261051b8daaec6c
LucLehmkuhl <70885301+LucLehmkuhl@users.noreply.github.com>
2022-04-28 12:22:05 +0200

    link register to the registered homepage view (#33)

--- implementation/frontend/Amicus_Frontend/Amicus_Frontend/RegisterView.swift
+++ implementation/frontend/Amicus_Frontend/Amicus_Frontend/RegisterView.swift
@@ -22,6 +22,7 @@ struct RegistrationView: View {
     
     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
     @State private var homeScreen = false
+    @State private var registeredScreen = false
     
     @State private var firstname: String = ""
     @State private var lastname: String = ""
@@ -32,77 +33,79 @@ struct RegistrationView: View {
     var body: some View {
         NavigationView {
             Color("Amicus1").ignoresSafeArea().overlay(
-            VStack(alignment: .leading) {
-                Group {
-                    Text("First Name").foregroundColor(Color("Amicus1"))
-                    TextField("First Name", text: $firstname, prompt: Text("Required"))
-                        .textFieldStyle(OvalTextFieldStyle())
-                        .foregroundColor(.red)
-                    
-                    Text("Last Name").foregroundColor(Color("Amicus1"))
-                    TextField("Last Name", text: $lastname, prompt: Text("Required").foregroundColor(Color("Amicus1")))
-                        .textFieldStyle(OvalTextFieldStyle())
-                    
-                    Text("Email").foregroundColor(Color("Amicus1"))
-                    TextField("Email", text: $email, prompt: Text("Required").foregroundColor(Color("Amicus1")))
-                        .textFieldStyle(OvalTextFieldStyle())
-                    
-                    Text("Password").foregroundColor(Color("Amicus1"))
-                    TextField("Password", text: $password, prompt: Text("Required").foregroundColor(Color("Amicus1")))
-                        .textFieldStyle(OvalTextFieldStyle())
-                    
-                    Text("Confirm Password").foregroundColor(Color("Amicus1"))
-                    TextField("Confirm Password", text: $passwordConfirm, prompt: Text("Required").foregroundColor(Color("Amicus1")))
-                        .textFieldStyle(OvalTextFieldStyle())
-                }
-                Group {
-                    Text("Do you want to add additional information such as a profile picture and more?")
-                        .foregroundColor(Color("Amicus1"))
-                    
-                    NavigationLink(destination: HomepageView()){
-                        Text("Click here")
-                    }.foregroundColor(Color("Amicus1"))
-                    
-                    NavigationLink(destination: HomepageView()) {
-                        Button("Register"){
-                            
-                        }.padding().buttonStyle(RegisterButton(background: Color("Amicus4")))
+                VStack(alignment: .leading) {
+                    Group {
+                        Text("First Name").foregroundColor(Color("Amicus1"))
+                        TextField("First Name", text: $firstname, prompt: Text("Required"))
+                            .textFieldStyle(OvalTextFieldStyle())
+                            .foregroundColor(.red)
+                        
+                        Text("Last Name").foregroundColor(Color("Amicus1"))
+                        TextField("Last Name", text: $lastname, prompt: Text("Required").foregroundColor(Color("Amicus1")))
+                            .textFieldStyle(OvalTextFieldStyle())
+                        
+                        Text("Email").foregroundColor(Color("Amicus1"))
+                        TextField("Email", text: $email, prompt: Text("Required").foregroundColor(Color("Amicus1")))
+                            .textFieldStyle(OvalTextFieldStyle())
+                        
+                        Text("Password").foregroundColor(Color("Amicus1"))
+                        TextField("Password", text: $password, prompt: Text("Required").foregroundColor(Color("Amicus1")))
+                            .textFieldStyle(OvalTextFieldStyle())
+                        
+                        Text("Confirm Password").foregroundColor(Color("Amicus1"))
+                        TextField("Confirm Password", text: $passwordConfirm, prompt: Text("Required").foregroundColor(Color("Amicus1")))
+                            .textFieldStyle(OvalTextFieldStyle())
                     }
-                }
-                .frame(alignment: .center)
-                //.padding(.horizontal, 100.0)
-                //.padding(.top, 10.0)
-            }
-            
-            .padding()
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                //Title
-                ToolbarItem(placement: .principal) {
-                    Text("Register")
-                        .font(.title.bold())
-                        .foregroundColor(Color("Amicus1"))
-                    .accessibilityAddTraits(.isHeader)}
-                //Home button and link to homepage
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button(action: {
-                        self.mode.wrappedValue.dismiss()
-                        homeScreen.toggle()
-                    }) {
-                        HStack {
-                            Image(systemName: "chevron.left")
-                                .foregroundColor(Color("Amicus1"))
-                            Text("Home")
-                                .foregroundColor(Color("Amicus1"))
+                    Group {
+                        Text("Do you want to add additional information such as a profile picture and more?")
+                            .foregroundColor(Color("Amicus1"))
+                        
+                        NavigationLink(destination: HomepageView()){
+                            Text("Click here")
+                        }.foregroundColor(Color("Amicus1"))
+                        
+                        NavigationLink(destination: HomepageView()) {
+                            Button("Register"){
+                                registeredScreen.toggle()
+                                
+                            }.padding().buttonStyle(RegisterButton(background: Color("Amicus4")))
                         }
                     }
+                    .frame(alignment: .center)
+                    //.padding(.horizontal, 100.0)
+                    //.padding(.top, 10.0)
                 }
-            }
-            .background(Color("Amicus3"))
-            .cornerRadius(20)
-            .padding()
-        )}
+                
+                    .padding()
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbar {
+                        //Title
+                        ToolbarItem(placement: .principal) {
+                            Text("Register")
+                                .font(.title.bold())
+                                .foregroundColor(Color("Amicus1"))
+                            .accessibilityAddTraits(.isHeader)}
+                        //Home button and link to homepage
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            Button(action: {
+                                self.mode.wrappedValue.dismiss()
+                                homeScreen.toggle()
+                            }) {
+                                HStack {
+                                    Image(systemName: "chevron.left")
+                                        .foregroundColor(Color("Amicus1"))
+                                    Text("Home")
+                                        .foregroundColor(Color("Amicus1"))
+                                }
+                            }
+                        }
+                    }
+                    .background(Color("Amicus3"))
+                    .cornerRadius(20)
+                    .padding()
+            )}
         .navigate(to: HomepageView(), when: $homeScreen)
+        .navigate(to: RegisteredHomepage(), when: $registeredScreen)
     }
 }
 
